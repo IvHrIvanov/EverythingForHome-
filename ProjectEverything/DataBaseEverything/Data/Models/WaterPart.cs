ad37eb4c8c03d215f12391d3a3fa568ea0705b4e
Ivan Ivanov <71284114+IvHrIvanov@users.noreply.github.com>
2022-02-28 16:54:06 +0200

    Create Project and Database

---
+++ ProjectEverything/DataBaseEverything/Data/Models/WaterPart.cs
@@ -0,0 +1,27 @@
+﻿using DataBaseevEverythingForHome.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseevEverythingForHome.Models
+{
+    public class WaterPart
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string PartWater { get; set; }
+        [Required]
+        public decimal Price { get; set; } = 0;
+        [Required]
+        public int Quantity { get; set; } = 0;
+
+        public int? ShopId { get; set; }
+        public Maggazine Shop { get; set; }
+    }
+}
